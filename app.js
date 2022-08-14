292fe99e9864dc977faaaaa123f7d4bc825c2ad3
Meng Li <mengli98git@gmail.com>
2022-08-13 23:21:10 -0400

    second commit

--- app.js
+++ app.js
@@ -7,22 +7,30 @@ class tabs {
         this.textArr[0].classList.remove("par")
         this.textArr[0].classList.add("selected");
         this.liArr[this.selected].classList.add("selectedTab");
-
+        
+        this.nav = document.querySelector(".nav");
 
         for (let i = 0; i < this.liArr.length; i++) {
-            this.liArr[i].addEventListener("click", () => {
-                //disable the previous selected
-                this.textArr[this.selected].classList.remove("selected");
-                this.textArr[this.selected].classList.add("par");
-                this.liArr[this.selected].classList.remove("selectedTab");
-
-                //enable the current selected
-                this.selected = i;
-                this.textArr[i].classList.remove("par")
-                this.textArr[i].classList.add("selected");
-                this.liArr[i].classList.add("selectedTab");
-            })
+            this.liArr[i].index = i;
         }
+
+        //冒泡法实现，只监听外层元素
+        // e.currentTarget 是外层元素，即nav ,  e.target is span by clicked
+        this.nav.addEventListener("click", (e) => {
+           const i = e.target.index;
+
+           //disable the previous selected
+           this.textArr[this.selected].classList.remove("selected");
+           this.textArr[this.selected].classList.add("par");
+           this.liArr[this.selected].classList.remove("selectedTab");
+
+           //enable the current selected
+           this.selected = i;
+           this.textArr[i].classList.remove("par")
+           this.textArr[i].classList.add("selected");
+           this.liArr[i].classList.add("selectedTab");
+
+        })
     }
 }
 
